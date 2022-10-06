bf390ca2a38ea30567e005f5de544cc742d59a26
cris <82042949+roseoque@users.noreply.github.com>
2022-10-06 12:26:05 +0100

    app2

--- app2/app/src/main/java/com/example/app2/MainActivity2.java
+++ app2/app/src/main/java/com/example/app2/MainActivity2.java
@@ -2,6 +2,8 @@ package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void Enviar(View view) {
+        TextView tx=findViewById(R.id.textView2);
+                tx.setText("Criss");
     }
 }
