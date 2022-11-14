562b42d415506d83084e0ef2749ebba514473c30
cris <82042949+roseoque@users.noreply.github.com>
2022-11-14 10:08:18 +0000

    aa
    aa

--- TRABALHO_1/app/src/main/java/com/example/trabalho_1/MainActivity.java
+++ TRABALHO_1/app/src/main/java/com/example/trabalho_1/MainActivity.java
@@ -1,10 +1,8 @@
-package com.example.app2;
+package com.example.trabalho_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void Enviar(View view) {
-            Intent intent=new Intent(this, MainActivity2.class);
-            startActivity(intent);
     }
 }
